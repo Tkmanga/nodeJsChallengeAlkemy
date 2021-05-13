e5a05b0808ccf1e1bc8fe170821b02193e4aa040
tacachojose <tacachojose@gmail.com>
2021-05-13 12:03:48 -0300

    agregando npm

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const colors = require('colors');
+
+const handleServer = (req,res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write(`<h1>Hola mundo desde node</h1>`);
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+
+server.listen(3000,()=>{
+    console.log('server on port 3000'.green)
+});
