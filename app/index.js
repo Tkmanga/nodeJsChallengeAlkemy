89e5cfa045c13164fe54f829fb44b28ed95a0a5b
tacachojose <tacachojose@gmail.com>
2021-05-13 18:24:50 -0300

    morgan

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
 
 const colors = require('colors');
+
 const exps = require('express');
+const morgan = require('morgan');
 
 const server = exps(); 
+
+/* function logger (req,res,next){
+    console.log(`Route Received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+ */
 server.use(exps.json());
+/* server.use(logger);  */
+server.use(morgan('dev')); 
+
+
+
 //Routing
+
+/* server.all('/user',(req, res,next)=>{
+    console.log('por aqui paso'); 
+    next();
+})
+ */
 
 server.get('/',(req, res) => {
     res.send(`<h1>Get req recibida</h1>`)
@@ -18,25 +37,33 @@ server.get('/user',(req, res) => {
         }
     );
 })
-server.post('/user',(req, res) => {
-    console.log(req.body)
-    res.send(`<h1>Post req recibida</h1>`)
+
+server.post('/user/:id',(req, res) => {
+    res.send(`<h1>Post recibida</h1>`)
+    console.log(req.body);
+    console.log(req.params);
     
 })
 server.put('/contact',(req, res) => {
     res.send(`<h1>Hola mundo</h1>`)
 })
+server.put('/user/:id',(req, res) => {
+    console.log(req.params)
+    res.send(`User ${req.params.id} actualizado`); 
+})
+
+
 
 server.delete('/delete',(req, res) => {
     res.send(`<h1>Pet delete recibida</h1>`)
+})
+server.delete('/user/:userId',(req, res) => {
+    res.send(`User ${req.params.userId} eliminated`)
 })
 
 server.listen(3001, ()=>{
     console.log(`Server on port 3001`.red)
 });
-
-
-
 
 /* 
 
