565ab518210623844cbd030c4cd3d729a65d9b68
tacachojose <tacachojose@gmail.com>
2021-05-13 17:48:38 -0300

    express inicio

--- app/index.js
+++ app/index.js
@@ -3,16 +3,46 @@ const colors = require('colors');
 const exps = require('express');
 
 const server = exps(); 
+server.use(exps.json());
+//Routing
 
 server.get('/',(req, res) => {
+    res.send(`<h1>Get req recibida</h1>`)
+})
+
+server.get('/user',(req, res) => {
+    res.json(
+        {
+            userName: 'jose',
+            lasTName: 'tacacho'
+        }
+    );
+})
+server.post('/user',(req, res) => {
+    console.log(req.body)
+    res.send(`<h1>Post req recibida</h1>`)
+    
+})
+server.put('/contact',(req, res) => {
     res.send(`<h1>Hola mundo</h1>`)
 })
 
-server.listen(3000, ()=>{
-    console.log(`Server on port 3000`.red)
+server.delete('/delete',(req, res) => {
+    res.send(`<h1>Pet delete recibida</h1>`)
+})
+
+server.listen(3001, ()=>{
+    console.log(`Server on port 3001`.red)
 });
 
-/* const http = require('http');
+
+
+
+/* 
+
+//Forma vieja 
+
+const http = require('http');
 const colors = require('colors');
 
 const handleServer = (req,res)=>{
