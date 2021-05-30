01743fd953fdd43c55623344580eb12a0df63edd
dario <tacachojose@gmail.com>
2021-05-30 14:51:27 -0300

    asd

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const colors = require("colors");
 
-const colors = require('colors');
-
-const exps = require('express');
-const morgan = require('morgan');
+const exps = require("express");
+const morgan = require("morgan");
 const port = 3001;
-const server = exps(); 
+const server = exps();
 
 /* function logger (req,res,next){
     console.log(`Route Received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
@@ -12,14 +11,14 @@ const server = exps();
 }
  */
 
-//settings 
-server.set('appName','Alk reto'); 
-server.set('port',3001);
+//settings
+server.set("appName", "Alk reto");
+server.set("port", 3001);
 
-//middlewares 
+//middlewares
 server.use(exps.json());
-server.use(morgan('dev'));
-server.set('view engine','ejs'); 
+server.use(morgan("dev"));
+server.set("view engine", "ejs");
 
 /* server.use(logger);  */
 
@@ -31,43 +30,40 @@ server.set('view engine','ejs');
 })
  */
 
-server.get('/',(req,res)=>{
-    const data = [{name:'jhon'},{name:'joe'},{name:'cameron'}]
-    res.render('index.ejs',{people:data});
+server.get("/", (req, res) => {
+  const data = [{ name: "jhon" }, { name: "joe" }, { name: "cameron" }];
+  res.render("index.ejs", { people: data });
 });
 
-server.get('/user',(req, res) => {
-    res.json(
-        {
-            userName: 'jose',
-            lasTName: 'tacacho'
-        }
-    );
-})
+server.get("/user", (req, res) => {
+  res.json({
+    userName: "jose",
+    lasTName: "tacacho",
+  });
+});
 
-server.post('/user/:id',(req, res) => {
-    res.send(`<h1>Post recibida</h1>`)
-    console.log(req.body);
-    console.log(req.params);
-    
-})
-server.put('/contact',(req, res) => {
-    res.send(`<h1>Hola mundo</h1>`)
-})
-server.put('/user/:id',(req, res) => {
-    console.log(req.params)
-    res.send(`User ${req.params.id} actualizado`); 
-})
-server.delete('/delete',(req, res) => {
-    res.send(`<h1>Pet delete recibida</h1>`)
-})
-server.delete('/user/:userId',(req, res) => {
-    res.send(`User ${req.params.userId} eliminated`)
-})
+server.post("/user/:id", (req, res) => {
+  res.send(`<h1>Post recibida</h1>`);
+  console.log(req.body);
+  console.log(req.params);
+});
+server.put("/contact", (req, res) => {
+  res.send(`<h1>Hola mundo</h1>`);
+});
+server.put("/user/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`User ${req.params.id} actualizado`);
+});
+server.delete("/delete", (req, res) => {
+  res.send(`<h1>Pet delete recibida</h1>`);
+});
+server.delete("/user/:userId", (req, res) => {
+  res.send(`User ${req.params.userId} eliminated`);
+});
 
-server.use(exps.static('public'))
-server.listen(server.get('port'), ()=>{
-    console.log(`Server on port ${server.get('port')}`.red);
+server.use(exps.static("public"));
+server.listen(server.get("port"), () => {
+  console.log(`Server on port ${server.get("port")}`.red);
 });
 
 /* 
